10e7e3d410bda9444bfc474e1cb732f239993daf
Amilissa Araneta <121836249+miss-amilissa@users.noreply.github.com>
2023-08-22 23:06:06 +0800

    feat: models and schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,7 +14,18 @@ router.get('/:id', (req, res)=> {
 })
 
 // POST a workout
-router.post('/', (req, res) =>{
+router.post('/', async(req, res) => {
+    const {title, reps, load} = req.body;
+
+    try {
+        const workout = await Workout.create({
+            title, reps, load
+        })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
     res.json({msg:"POST a new workout"})
 })
 
